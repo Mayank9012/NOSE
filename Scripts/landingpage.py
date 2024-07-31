3b7354291727d24d38d54de950fbe20b0e26cff0
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-07-31 18:14:14 +0530

    Update landingpage.py

--- Scripts/landingpage.py
+++ Scripts/landingpage.py
@@ -5,7 +5,7 @@ import webbrowser
 st.set_page_config(initial_sidebar_state='auto' )
 st.write("#")
 
-with open('/home/maya/NoSE/nose/CSS/landingpage.css') as f:
+with open('CSS/landingpage.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 # Confirmation dialog
 @st.experimental_dialog("Check Status")
