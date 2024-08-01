04d85b7f4ba5d4f8a5460dd197b2f089476e9ade
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-08-01 16:16:41 +0530

    Update sidebar_nose.py

--- Scripts/sidebar_nose.py
+++ Scripts/sidebar_nose.py
@@ -31,7 +31,7 @@ def sidebar(step1, step2, step3, step4, step5, step6):
         }
 
         .stProgress>div>div {
-            width: 100%;
+            width: 10vh;
             /* Adjust width of each progress circle */
             transform: translateX(-5px) translateY(5px);
         }
@@ -40,12 +40,12 @@ def sidebar(step1, step2, step3, step4, step5, step6):
             background-color: #cac1c3;
             transform: rotate(90deg);
             justify-content: center;
-            width: 20vh;
+            width: 10vh;
         }
 
         .stProgress>div>div>div>div {
             background-color: #69ffd2;
-            widht:100%;
+            widht:10vh;
         }
 
         .stStepCircle {
