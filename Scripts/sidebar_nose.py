12345d1196224a6175858833ec54a9c6855d18fd
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-08-02 16:25:30 +0530

    Update sidebar_nose.py

--- Scripts/sidebar_nose.py
+++ Scripts/sidebar_nose.py
@@ -153,7 +153,7 @@ def sidebar(step1, step2, step3, step4, step5, step6):
             bottom: 10%;
         }
         .index{
-            transform: translateY(-10px);
+            transform: translateY(-6px);
             font-weight:bold;
         }
     </style>
