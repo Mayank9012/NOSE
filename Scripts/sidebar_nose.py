3e6995bc544c61582f5857bdf4e30713b1be202f
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-08-02 11:22:18 +0530

    Update sidebar_nose.py

--- Scripts/sidebar_nose.py
+++ Scripts/sidebar_nose.py
@@ -33,7 +33,7 @@ def sidebar(step1, step2, step3, step4, step5, step6):
         .stProgress>div>div {
             width: 100vh;
             /* Adjust width of each progress circle */
-            transform: translateX(-116px) translateY(-18px);
+            transform: translateX(-121px) translateY(-18px);
         }
 
         .stProgress>div>div>div {
