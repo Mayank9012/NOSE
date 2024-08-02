daa689932c867b345ee0cde61d45d8e4b5bfed34
Mayank Tanwar <166604439+Mayank9012@users.noreply.github.com>
2024-08-02 11:34:01 +0530

    Update sidebar_nose.py

--- Scripts/sidebar_nose.py
+++ Scripts/sidebar_nose.py
@@ -66,8 +66,8 @@ def sidebar(step1, step2, step3, step4, step5, step6):
 
         .stStepCircle1 {
             background-color: #023b2a;
-            width: 4vh;
-            height: 4vh;
+            width: 4rem;
+            height: 4rem;
             border-radius: 50%;
             display: inline-block;
             margin-bottom: 1vh;
@@ -80,8 +80,8 @@ def sidebar(step1, step2, step3, step4, step5, step6):
 
         .stStepCircle2 {
             background-color: #023b2a;
-            width: 4vh;
-            height: 4vh;
+            width: 4rem;
+            height: 4rem;
             border: 4px solid #fff;
             border-radius: 50%;
             display: flex;
